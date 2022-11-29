d83dcefc4a431a20206d66fb8548db50ff8cb6eb
Ivan test <ivan02081983s@ya.ru>
2022-11-29 07:36:36 +0300

    delite the

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public int calculate(int theWeight, int growth) {
+    public int calculate(int weight, int growth) {
         double heightMeter = (double) growth / 100;
-        double index = theWeight / (heightMeter * heightMeter);
+        double index = weight / (heightMeter * heightMeter);
         int indexInt = (int) index;
         return indexInt;
     }
